0966b83a5a08eae8c2b43794845d36deca44cf1a
saihnii4 <81787757+saihnii4@users.noreply.github.com>
2023-11-18 18:03:40 +0800

    learning rust

---
+++ README.md
@@ -0,0 +1,14 @@
+# mr white
+
+cool discord bot supporting STEM servers
+
+## (planned) features
+
+- molecule and species codex
+- competitive programming support (cses, codeforces, spoj)
+- built-in POTD
+
+## credits
+
+- nablasleep (me)
+- the rust and serenity documentation (wouldn't have made it without them)
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,37 @@
 use std::env;
+use dotenv::dotenv;
 
 use serenity::async_trait;
+use serenity::model::user::OnlineStatus;
+use serenity::model::gateway::Activity;
 use serenity::prelude::*;
 use serenity::model::channel::Message;
 use serenity::framework::standard::macros::{command, group};
 use serenity::framework::standard::{StandardFramework, CommandResult};
+use serenity::model::gateway::*;
 
 #[group]
-#[commands(ping)]
+#[commands(ping, xd)]
 struct General;
 
 struct Handler;
 
 #[async_trait]
-impl EventHandler for Handler {}
+impl EventHandler for Handler {
+    async fn ready(&self, ctx: Context, _ : Ready) {
+        ctx.set_presence(Some(Activity::watching("stuff xd")), OnlineStatus::DoNotDisturb).await;
+    }
+}
 
 #[tokio::main]
 async fn main() {
+    dotenv().ok();
     let framework = StandardFramework::new()
         .configure(|c| c.prefix("~")) // set the bot's prefix to "~"
         .group(&GENERAL_GROUP);
 
     // Login with a bot token from the environment
-    let token = env::var("DISCORD_TOKEN").expect("token");
+    let token = env::var("TOKEN").expect("token");
     let intents = GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT;
     let mut client = Client::builder(token, intents)
         .event_handler(Handler)
@@ -40,5 +49,12 @@ async fn main() {
 async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
     msg.reply(ctx, "Pong!").await?;
 
+    Ok(())
+}
+
+#[command]
+async fn xd(ctx: &Context, msg: &Message) -> CommandResult {
+    println!("{} said \"{}\"", msg.author.name, msg.content);
+    msg.reply(ctx, "Logged xd").await?;
     Ok(())
 }
