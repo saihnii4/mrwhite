74c8fdd4ebe3bbf4bd7eefdf3d03db915c1ccc31
saihnii4 <81787757+saihnii4@users.noreply.github.com>
2024-03-17 21:30:36 +0800

    feat: learning serenity

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
-use std::env;
 use dotenv::dotenv;
+use std::env;
 
 use serenity::async_trait;
-use serenity::model::user::OnlineStatus;
-use serenity::model::gateway::Activity;
-use serenity::prelude::*;
-use serenity::model::channel::Message;
 use serenity::framework::standard::macros::{command, group};
-use serenity::framework::standard::{StandardFramework, CommandResult};
+use serenity::framework::standard::{CommandResult, StandardFramework};
+use serenity::model::channel::Message;
+use serenity::model::gateway::Activity;
 use serenity::model::gateway::*;
+use serenity::model::user::OnlineStatus;
+use serenity::prelude::*;
 
 #[group]
 #[commands(ping, xd)]
@@ -18,9 +18,21 @@ struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {
-    async fn ready(&self, ctx: Context, _ : Ready) {
-        ctx.set_presence(Some(Activity::watching("stuff xd")), OnlineStatus::DoNotDisturb).await;
+    async fn ready(&self, ctx: Context, _: Ready) {
+        ctx.set_presence(
+            Some(Activity::watching("stuff xd")),
+            OnlineStatus::DoNotDisturb,
+        )
+        .await;
     }
+
+    // async fn message(&self, ctx: Context, msg: Message) {
+    //     if msg.is_own(&ctx.cache) {
+    //         return
+    //     };
+    //     let _ = msg.reply(&ctx, "haiii :3").await;
+    //     let _ = msg.pin(&ctx).await;
+    // }
 }
 
 #[tokio::main]
@@ -46,6 +58,7 @@ async fn main() {
 }
 
 #[command]
+#[aliases("pong")]
 async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
     msg.reply(ctx, "Pong!").await?;
 
